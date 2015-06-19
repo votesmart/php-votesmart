7fc55bfa81f16f8eb4d58c8b123aa4ffbf3a2ba1
Derek Rosenzweig <derek.rosenzweig@gmail.com>
2015-06-19 19:36:35 -0400

    Added .gitignore file to ignore PhpStorm project files. Cleaned up formatting of VoteSmart class.

--- VoteSmart.php
+++ VoteSmart.php
@@ -17,108 +17,92 @@ require_once("config.php");
  * contribution for the slimmed down version of this lib.
  * 
  */
-class VoteSmart {
-        
-        protected $iface;          // Interface(URL) used to gain the data
-        protected $xml;            // Raw XML
-        protected $xmlObj;         // SimpleXML object
-        
-        /**
-         * function __construct
-         * 
-         * Initialize object(optional)
-         * 
-         * @param string $method optional 'CandidateBio.getBio'
-         * @param array $args optional Array('candidateId' => '54321')
-         */
-        public function __construct($method = null, $args = null) {
-                
-                if ($method && $args) {
-                        
-                        $this->query($method, $args);
-                        
-                }
-                
+class VoteSmart
+{
+    protected $iface;          // Interface(URL) used to gain the data
+    protected $xml;            // Raw XML
+    protected $xmlObj;         // SimpleXML object
+
+    /**
+     * function __construct
+     *
+     * Initialize object(optional)
+     *
+     * @param string $method optional 'CandidateBio.getBio'
+     * @param array $args optional Array('candidateId' => '54321')
+     */
+    public function __construct($method = null, $args = null)
+    {
+        if ($method && $args) {
+            $this->query($method, $args);
         }
-        
-        /**
-         * function getXml
-         * 
-         * Return raw XML string
-         * 
-         * @return string
-         */
-        public function getXml() {
-                
-                return $this->xml;
-                
+    }
+
+    /**
+     * function query
+     *
+     * Query API backend and return SimpleXML object.  This
+     * function can be reused repeatedly
+     *
+     * @param string $method CandidateBio.getBio'
+     * @param array $args Array('candidateId' => '54321')
+     * @return object SimpleXMLElement
+     */
+    public function query($method, $args = Array())
+    {
+        $terms = "";
+
+        if (! empty($args)) {
+            foreach ($args as $n => $v) {
+                $terms .= '&' . $n . '=' . $v;
+            }
         }
-        
-        /**
-         * function getXmlObj
-         * 
-         * Return SimpleXML object
-         * 
-         * @return object SimpleXMLElement
-         */
-        public function getXmlObj() {
-                
-                return $this->xmlObj;
-                
+        $this->iface = _APISERVER_ . "/" . $method . "?key=" . _KEY_ . "&o=" . _OUTPUT_  . $terms;
+
+        if (! $this->xml = file_get_contents($this->iface)) {
+            return false;
+        } else {
+            // Let's use the SimpleXML to drop the whole XML
+            // output into an object we can later interact with easilly
+            $this->xmlObj = new SimpleXMLElement($this->xml, LIBXML_NOCDATA);
+
+            return $this->xmlObj;
         }
-        
-        /**
-         * function getIface
-         * 
-         * Return string of URL queried
-         * 
-         * @return string
-         */
-        public function getIface() {
-                
-                return $this->iface;
-                
-        }
-        
-        /**
-         * function query
-         * 
-         * Query API backend and return SimpleXML object.  This
-         * function can be reused repeatedly
-         * 
-         * @param string $method CandidateBio.getBio'
-         * @param array $args Array('candidateId' => '54321')
-         * @return object SimpleXMLElement
-         */
-        public function query($method, $args = Array()) {
-                
-                $terms = "";
-                
-                if(!empty($args)) {
-				
-			foreach($args as $n => $v) {
-				
-				$terms .= '&' . $n . '=' . $v;
-				 
-			}
-		}
-		
-		$this->iface = _APISERVER_ . "/" . $method . "?key=" . _KEY_ . "&o=" . _OUTPUT_  . $terms;
-		
-                if (!$this->xml = file_get_contents($this->iface)) {
-                		
-                        return false;
-                		
-                } else {
-                		
-                        // Let's use the SimpleXML to drop the whole XML
-                        // output into an object we can later interact with easilly
-                        $this->xmlObj = new SimpleXMLElement($this->xml, LIBXML_NOCDATA);
-                        
-                        return $this->xmlObj;
-                		
-                }
-                
-        }
-	
+    }
+
+    /**
+     * function getXml
+     *
+     * Return raw XML string
+     *
+     * @return string
+     */
+    public function getXml()
+    {
+        return $this->xml;
+    }
+
+    /**
+     * function getXmlObj
+     *
+     * Return SimpleXML object
+     *
+     * @return object SimpleXMLElement
+     */
+    public function getXmlObj()
+    {
+        return $this->xmlObj;
+    }
+
+    /**
+     * function getIface
+     *
+     * Return string of URL queried
+     *
+     * @return string
+     */
+    public function getIface()
+    {
+        return $this->iface;
+    }
 }
